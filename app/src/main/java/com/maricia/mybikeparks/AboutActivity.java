6de59f77750c5e47edd36c4a9f041d74402c0d3b
maricia <maricia.a.alleman@gmail.com>
2018-03-28 11:14:33 -0500

    updated Gradle to lastest version

--- app/src/main/java/com/maricia/mybikeparks/AboutActivity.java
+++ app/src/main/java/com/maricia/mybikeparks/AboutActivity.java
@@ -25,8 +25,8 @@ public class AboutActivity extends AppCompatActivity{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
 
-        tpversion = this.findViewById(R.id.tpversion);
-        tpauthor = this.findViewById(R.id.tpauthor);
+        tpversion = (TextView) this.findViewById(R.id.tpversion);
+        tpauthor = (TextView) this.findViewById(R.id.tpauthor);
 
         tpversion.setText(MessageFormat.format("version:{0} Build: {1}",versionName,versionCode));
 
