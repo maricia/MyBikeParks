adecae389a0c3decadd8cab5bf36c6d606d012d2
maricia <maricia.a.alleman@gmail.com>
2018-04-20 21:50:11 -0500

    add onPause, onResume, and changelistner for the prefences fragment

--- app/src/main/java/com/maricia/mybikeparks/SettingsFragment.java
+++ app/src/main/java/com/maricia/mybikeparks/SettingsFragment.java
@@ -3,6 +3,7 @@ package com.maricia.mybikeparks;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.Preference;
 import android.preference.PreferenceFragment;
 import android.preference.PreferenceManager;
 import android.support.annotation.Nullable;
@@ -10,7 +11,7 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class SettingsFragment extends PreferenceFragment  {
+public class SettingsFragment extends PreferenceFragment implements SharedPreferences.OnSharedPreferenceChangeListener {
 
     String theDate;
     String walkTime;
@@ -49,5 +50,28 @@ public class SettingsFragment extends PreferenceFragment  {
         //String latitudeString = pref.getString("Latitude", "0");
         // double latitude = Double.parseDouble(latitudeString);
        }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        getPreferenceScreen().getSharedPreferences()
+                .registerOnSharedPreferenceChangeListener(this);
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        getPreferenceScreen().getSharedPreferences()
+                .unregisterOnSharedPreferenceChangeListener(this);
+    }
+
+    @Override
+    public void onSharedPreferenceChanged(SharedPreferences
+                                                  sharedPreferences, String key) {
+        if (key.equals("food_preference")) {
+            Preference foodPref = findPreference(key);
+            foodPref.setSummary(sharedPreferences.getString(key, ""));
+        }
+    }
 
     }
